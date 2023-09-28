f805a485d640df7bb3233b6914eaa0a467cd2075
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-09-28 20:49:21 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,19 +2,8 @@ let imgElement = document.getElementById('cookie');
 let counterElement = document.getElementById('clicker__counter');
 let clickCount = 0;
 
-imgElement.addEventListener('click', function() {
-    
-    clickCount++;
-    counterElement.textContent = clickCount;
-}
-);
 
 imgElement.addEventListener('click', function () {
-    
-    if (clickCount % 2 === 0) {
-        imgElement.width = 150;
-    } else {
-        imgElement.width = 200;
-    }
+    imgElement.width = ++counterElement.textContent % 2 === 0 ? 200 : 250;
 });
 
