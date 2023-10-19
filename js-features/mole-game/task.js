e4d09c7fc55c9c31c7474148368689470372890c
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-10-19 13:35:54 +0300

    Update task.js

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,28 +5,31 @@ let lost = document.getElementById("lost");
 let deadMoly = 0;
 let lostMoly = 0;
 
-for (let i = 1; i <= 9; i++) {
-  getHole(i).onclick = function () {
-    if (this.classList.contains("hole_has-mole")) {
-      deadMoly++;
+function gameOwer(dead, lost) {
+    if (deadMoly === 10) {
+        alert('Вы выиграли')
+        deadMoly = 0;
+        lostMoly = 0;
         dead.textContent = deadMoly;
-        if (deadMoly === 10) {
-            alert('Вы выиграли')
-            deadMoly = 0;
-            lostMoly = 0;
-            dead.textContent = deadMoly;
-            lost.textContent = lostMoly;
-        }
-    } else {
-      lostMoly++;
         lost.textContent = lostMoly;
-        if (lostMoly >= 5) {
+    }else if(lostMoly >= 5) {
             alert('Вы проиграли')
             deadMoly = 0;
             lostMoly = 0;
             dead.textContent = deadMoly;
             lost.textContent = lostMoly;
         }
-    }
-  };
 }
+
+for (let i = 1; i <= 9; i++) {
+    getHole(i).onclick = function () {
+        if (this.classList.contains("hole_has-mole")) {
+            deadMoly++;
+            dead.textContent = deadMoly;
+        } else {
+            lostMoly++;
+            lost.textContent = lostMoly;
+        }
+        gameOwer(deadMoly, lostMoly);
+    };
+};
