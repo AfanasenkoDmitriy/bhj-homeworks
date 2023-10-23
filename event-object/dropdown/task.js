ee9502c54201eecd4651c105d35091d40a0cb073
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2023-10-23 20:36:37 +0300

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,22 @@
+let dropdown = document.querySelector(".dropdown__value");
+
+function openAndCloseDropdown() {
+    let dropdownList = document.querySelector('.dropdown__list')
+    dropdownList.classList.toggle("dropdown__list_active")
+}
+
+dropdown.addEventListener("click", openAndCloseDropdown);
+
+let dropdownItem = Array.from(document.querySelectorAll(".dropdown__link"));
+
+dropdownItem.forEach(function (item) {
+    item.onclick = function () {
+        document.querySelector('.dropdown__value').textContent = item.textContent;
+    }
+})
+
+dropdownItem.forEach(function(element) {
+    element.addEventListener('click', (e) => {
+        e.preventDefault()
+    })
+})
