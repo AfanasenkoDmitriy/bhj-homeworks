caf09be88c83b7498678ef492bd09448fded3fd1
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2024-02-20 18:52:56 +0200

    Update task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,25 +1,58 @@
-const tabs = document.getElementById('tabs1');
+const tabsParrent = document.getElementById('tabs1');
 const tab = Array.from(document.querySelectorAll('.tab'));
+const tabContent = document.querySelectorAll(".tab__content");
 
-let selected;
+function hideTabContent() {
+  tabContent.forEach((item) => {
+      item.classList.remove("tab__content_active");
+  });
 
-tabs.onclick = function (event) {
-    let target = event.target;
-    
-    if (!target.classList.contains('tab')) {
-        return
-    }
-    highlits(target);
+  
+  tab.forEach((item) => {
+    item.classList.remove("tab_active");
+  });
 }
 
-function highlits(tab) {
-    if (selected) {
-        selected.classList.remove("tab_active");
-    }
-    selected = tab;
-    selected.classList.add("tab_active");
-    
+function showTabContent(i = 0) {
+  tabContent[i].classList.add("tab__content_active");
+  tab[i].classList.add("tab_active");
 }
+
+hideTabContent();
+showTabContent();
+
+tabsParrent.addEventListener("click", (event) => {
+  const target = event.target; 
+
+  if (target && target.classList.contains("tab")) {
+    tab.forEach((item, i) => {
+      if (target == item) {
+        hideTabContent();
+        showTabContent(i);
+      }
+    });
+  }
+});
+
+
+
+
+// tabs.onclick = function (event) {
+//     let target = event.target;
+    
+//     if (!target.classList.contains('tab')) {
+//         return;
+//     }
+//     highlits(target);
+// }
+
+// function highlits(tab) {
+//     if (selected) {
+//         selected.classList.remove("tab_active");
+//     }
+//     selected = tab;
+//     selected.classList.add("tab_active");
+// }
 
 
 
