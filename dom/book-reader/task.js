55197666caf956f43c8474cfcc26e5e794194cdf
“DmitriyAfanasenko” <94554795+AfanasenkoDmitriy@users.noreply.github.com>
2024-03-05 21:13:38 +0200

    Update task.js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,12 +1,31 @@
-const book = document.querySelector('.book');
-const fontSize = document.querySelectorAll('.font-size')
-console.log(fontSize)
-fontSize.forEach((e) => {
-    e.addEventListener('click', () => {
-        if (EventTarget.classList.contains("font-size_small")) {
-            book.forEach((s) => {
-              s.classList.add("book_fs-small");
-          });
-        }
-    })
-})
+const book = document.querySelector(".book");
+const fontSize = Array.from(document.querySelectorAll(".font-size"));
+let value = "";
+
+fontSize.forEach((elem) => {
+  elem.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const activeElement = fontSize.find((el) =>
+      el.classList.contains("font-size_active")
+    );
+
+    activeElement.classList.remove("font-size_active");
+
+    elem.classList.add("font-size_active");
+
+    if (elem.hasAttribute("data-size")) {
+      value = elem.getAttribute("data-size");
+    }
+
+    book.classList.forEach((item) => {
+      if (item.startsWith("book_fs-")) {
+        book.classList.remove(item);
+      } else if (value === "big") {
+        book.classList.add("book_fs-big");
+      } else {
+        book.classList.add("book_fs-small");
+      }
+    });
+  });
+});
